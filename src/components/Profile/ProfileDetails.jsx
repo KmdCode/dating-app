d41b52a2d889717b8294dc54eaeccafa8715caaf
Jacob Tshepo <karabomodise260@gmail.com>
2024-08-05 13:52:00 +0200

    delete profile button added

--- src/components/Profile/ProfileDetails.jsx
+++ src/components/Profile/ProfileDetails.jsx
@@ -59,6 +59,10 @@ const ProfileDetails = () => {
       <Link to="/update-profile" className="self-start bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
         Update Profile
       </Link>
+      <button className="self-start bg-blue-500 text-white py-2 px-4 ml-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+        Delete Profile
+      </button>
+      
       </div>
     </div>
   );
