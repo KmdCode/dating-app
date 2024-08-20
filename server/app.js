90bc69489ca2549292ca208ff53ce878cf43d9a8
Jacob Tshepo <karabomodise260@gmail.com>
2024-08-20 21:47:04 +0200

    server changes made

--- server/app.js
+++ server/app.js
@@ -2,10 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-
+dotenv.config({path: './config/config.env'})
 const app = express()
 app.use(express.json())
-
 
 dotenv.config({path: './config/config.env'})
 
@@ -19,7 +18,14 @@ mongoose.connect(DB, {
 })
 
 
-const port = 8000
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        message: 'connection successful'
+    })
+})
+
+const port = process.env.PORT
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
