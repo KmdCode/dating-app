bfc5508f95390ff8ed04804a13797cde631afd78
Jacob Tshepo <karabomodise260@gmail.com>
2024-08-20 21:10:12 +0200

    db connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 
 const app = express()
 app.use(express.json())
+
+
+dotenv.config({path: './config/config.env'})
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+}).then(con=>{
+    //console.log(con.connections)
+    console.log('DB connection successfull')
+})
+
 
 const port = 8000
 
---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({path: './config.env'})
+
+//const DB = process.env.DB.replace('<password>', process.env.DATABASE_PASS)
+
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+}).then(con=>{
+    console.log(con.connections)
+    console.log('DB connection successfull')
+})
+
+module.exports = db;
