3d1d00f6833e52a95ec563d6da8b56df78937861
Jacob Tshepo <karabomodise260@gmail.com>
2024-08-22 13:08:12 +0200

    User model done

---
+++ server/models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\d{8}@swave\.smu\.ac\.za$/, 'Please use your SMU provided email address'],
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    profileCompleted:{
+        type: Boolean,
+        default: false
+    },
+    otp:{
+        code: String,
+        expiresAt: Date,
+    },
+    userName:{
+        type: String,
+        required: [true, 'User name is required']
+    },
+    age:{
+        type: Number,
+        required: [true, 'Age is required']
+    },
+    residence:{
+        type: String,
+        required: [true, 'Student residence is required']
+    },
+    courseOfStudy:{
+        type: String,
+        require: [true, 'Course of study is required']
+    },
+    levelOfStudy:{
+        type: String,
+        required: [true, 'Level of study is required']
+    },
+    relationshipGoals:{
+        type: String,
+        required: [true, 'Relationship goal required']
+    },
+    role:{
+        type: String,
+        enum: ['applicant', 'advertiser']
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User;
