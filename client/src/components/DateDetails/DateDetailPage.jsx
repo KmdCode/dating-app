7506e75ae592ab7ba7513363ac3e93572e29f1f3
KmdCode <karabomodise260@gmail.com>
2024-09-24 18:28:08 +0200

    Application button removed for advertisers

--- client/src/components/DateDetails/DateDetailPage.jsx
+++ client/src/components/DateDetails/DateDetailPage.jsx
@@ -7,6 +7,7 @@ import ApplyButton from './ApplyButton';
 
 const DateDetailPage = () => {
   const { dateId } = useParams(); 
+  const [role, setRole] = useState(null);
   const [dateSetup, setDateSetup] = useState(null); 
   const [loading, setLoading] = useState(true); 
 
@@ -30,6 +31,27 @@ const DateDetailPage = () => {
     fetchDateSetup(); 
   }, [dateId]);
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      axios.get('http://127.0.0.1:8000/api/v1/user/profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => {
+          const userRole = res.data.data.user.role;
+          setRole(userRole);
+          console.log(`user role is ${userRole}`)
+        })
+        .catch((err) => {
+          console.error('Error fetching user profile:', err);
+        });
+    }
+  }, []);
+
+
   if (loading) {
     return <div>Loading...</div>; 
   }
@@ -41,7 +63,10 @@ const DateDetailPage = () => {
   return (
     <div className="date-detail-page mx-auto p-4 bg-red-600 shadow-md mt-0 pb-96 pt-20">
       <DateDetail dateSetup={dateSetup} /> 
-      <ApplyButton dateId={dateId}/>
+      {role === 'applicant' && (
+        <ApplyButton dateId={dateId}/>
+      )}
+      
     </div>
   );
 };
